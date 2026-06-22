84f71b1bf3505ff1a352b7911191bbdc2bd0bdc9
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-22 12:43:02 +0530

    added new Class and Better Code Improvement

--- backend/injestion/Extract.py
+++ backend/injestion/Extract.py
@@ -2,9 +2,46 @@ from langchain_unstructured import UnstructuredLoader
 from typing import List
 from langchain.schema import Document
 from io import BytesIO
+import re
 
-def extract_text(file_content: BytesIO, filename: str) -> str:
-    loader = UnstructuredLoader(file=file_content, metadata_filename=filename)
-    docs: List[Document] = loader.load()
-    full_text = " ".join([doc.page_content for doc in docs])
-    return full_text
+
+class DocumentProcessor:
+    
+    def __init__(self):
+           self.loader = UnstructuredLoader
+
+    def load_document(self, document):
+      loader = self.loader(file_path=document, strategy="hi_res", languages=["eng"])
+      documents = loader.load()
+      return documents
+    
+    def clean_documents(self, documents):
+        clean_docs = [] #Langchain Document Objects are Gonna be Stored Here
+        for doc in documents:
+            metadata = doc.metadata
+            text = doc.page_content
+            text = re.sub(r'\s+ \d+', ' ', text)
+            clean_docs.append(
+                Document(
+                    page_content=text,
+                    metadata=metadata
+                )
+            )
+        
+        return clean_docs
+    
+    def extract(self, clean_docs):
+        
+            
+
+
+
+
+
+
+
+#def extract_text(file_content: BytesIO, filename: str) -> str:
+    #loader = UnstructuredLoader(file=file_content, metadata_filename=filename)
+    #docs: List[Document] = loader.load()
+    #full_text = " ".join([doc.page_content for doc in docs])
+    #return full_text
