d31ef475661e96c42a5c1b336ea9d42695983adc
Channa2146@gmail.com <Channa2146@gmail.com>
2026-07-08 11:00:04 +0530

    add dependacy injestionService

--- backend/services/dependancyService/dependancyService.py
+++ backend/services/dependancyService/dependancyService.py
@@ -3,12 +3,17 @@ from services.embeddingService import HFEmbeddingModel
 from services.inferenceService import InferenceService
 from services.modelService import modelService
 from services.vectorDBService import VectorService
+from sentence_transformers import SentenceTransformer
+from langchain_unstructured import UnstructuredLoader
+from langchain.chains import RetrievalQA
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 class DependencyContainer:
     def __init__(self):
-        self.injestionService = DocumentProcessor().load_Document
-        self.embeddingService = HFEmbeddingModel()
-        self.inferenceService = InferenceService()
+        self.injestionService = DocumentProcessor(UnstructuredLoader, RecursiveCharacterTextSplitter())
+        self.embeddingService = HFEmbeddingModel(SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2'))
+        self.inferenceService = InferenceService().create_qa_chain(RetrievalQA, self.embeddingService)
         self.modelService = modelService()
         self.vectorService = VectorService()
         
