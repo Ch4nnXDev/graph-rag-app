87bbc59c04d2e128ab86da211176c14a2f5a5512
Channa2146@gmail.com <Channa2146@gmail.com>
2026-07-09 11:16:10 +0530

    added upload doc function

---
+++ backend/services/documentService/documentService.py
@@ -0,0 +1,13 @@
+from backend.services.dependancyService import DependancyContainer
+
+class DocumentService:
+    def __init__(self):
+        self.dependancy = DependancyContainer()
+        
+    def upload_docs(self, doc):
+        docs = self.dependancy.injestionService().load_document(doc)
+        clean_docs = self.dependancy.injestionService.clean_documents(docs)
+        chunked = self.dependancy.injestionService.chunk_documents(clean_docs)
+        return self.dependancy.vector_store.add_documents(chunked)
+    
+    
