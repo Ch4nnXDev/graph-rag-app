194859704d4c27e27a7942b64df5f40d7d561f30
Channa2146@gmail.com <Channa2146@gmail.com>
2026-07-11 22:09:23 +0530

    setup router and chat_routes

--- backend/api/routes/chat_routes.py
+++ backend/api/routes/chat_routes.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
+from ...services.chatService import chatService
 
+chat = chatService()
 
 router = APIRouter()
 
--- backend/services/main.py
+++ backend/services/main.py
@@ -1,14 +1,6 @@
 from fastapi import FastAPI
 from chatService import ChatService
+from api.routes.chat_routes import router as chat_router
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {
-        "message": "Welcome"
-    }
-
-
-@app.post("/chat")
-def chat(query):
-    
+app.include_router(chat_router)
