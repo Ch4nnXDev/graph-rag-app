564ac0d1dd044e75a4124ca16e8ff4415ddc452d
Channa2146@gmail.com <Channa2146@gmail.com>
2026-07-15 07:29:16 +0530

    added test route

--- backend/services/main.py
+++ backend/services/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
-from chatService import ChatService
 from api.routes.chat_routes import router as chat_router
+
 app = FastAPI()
 
 app.include_router(chat_router)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Start"
+    }
